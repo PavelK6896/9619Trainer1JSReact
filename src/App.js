9d63d6310809ec4b5638d3da1de4fbcf602f54e5
PavelK6896 <43148722+PavelK6896@users.noreply.github.com>
2020-05-30 23:34:38 +0300

    "time state between pressing"

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useReducer} from 'react';
 
-const initialState = {count: 0, text: ""};
+const initialState = {count: 0, text: "1", data: 0, data2: 0, time1: 0};
 
 function reducer(state, action) {
     switch (action.type) {
@@ -9,34 +9,42 @@ function reducer(state, action) {
         case 'decrement':
             return {count: state.count - 1, text: state.text};
         case 'key':
-            return {count: state.count, text: state.text + action.key};
+
+            return {
+                count: action.count,
+                text: state.text + action.key,
+                data: action.data,
+                data2: action.data2,
+                time1: action.time1
+            };
         default:
             throw new Error();
     }
 }
-
 export function App() {
     const [state, dispatch] = useReducer(reducer, initialState);
+
     useEffect(() => {
         const handleEsc = (event) => {
-            dispatch({type: 'key', key: event.key})
-            console.log(event.keyCode)
+            if (state.data === 0){
+                dispatch({type: 'key',count: state.count + 1,  key: event.key, data: new Date().getTime(), data2: 0, time1: 0 })
+            }else{
+                const dt = new Date().getTime() - state.data
+                dispatch({type: 'key', count: state.count + 1, key: event.key, data: state.data, data2: dt, time1: dt/state.count})
+            }
         };
         window.addEventListener('keydown', handleEsc);
         return () => {
             window.removeEventListener('keydown', handleEsc);
         };
-    }, []);
+    }, [state]);
 
     return (
         <>
-            <p>Count: {state.count}</p>
+            <p>Count: {state.count} this is time {Math.round(state.time1) } ms between pressing </p>
             <button onClick={() => dispatch({type: 'decrement'})}>-</button>
             <button onClick={() => dispatch({type: 'increment'})}>+</button>
             <p>Text: {state.text}</p>
         </>
     );
 }
-
-
-
