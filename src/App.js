f1cc897babb866f3a8d8120666e7453b83ed77cd
PavelK6896 <43148722+PavelK6896@users.noreply.github.com>
2020-05-30 19:02:14 +0300

    "just input "

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            clicked: false,
+            text: "class App extends React.Component {",
+            text2: "class App extends React.Component {",
+            colorText: "black",
+            ch: 0
+        }
+    }
+
+    render() {
+
+        const text1 = (event) => {
+            if (this.state.text.length !== 0) {
+                if (event.target.value[event.target.value.length - 1] === this.state.text[0]) {
+
+                    if (this.state.ch < event.target.value.length) {
+                        console.log(this.state.ch, event.target.value.length)
+
+                        this.setState(
+                            {
+                                text: this.state.text.substr(1),
+                                colorText: "black",
+                                ch: event.target.value.length
+                            })
+                    }else{
+                        console.log(this.state.ch, event.target.value.length)
+                        this.setState(
+                            {
+                                ch: event.target.value.length
+                            })
+                    }
+
+                } else {
+                    this.setState(
+                        {
+                            colorText: "red"
+                        })
+                }
+            }
+        }
+
+        return (
+            <div style={{
+                display: "flex",
+                flexDirection: "column",
+                margin: "auto",
+                maxWidth: "80%",
+            }}>
+                <h1> <pre>
+                   {this.state.text}</pre>
+                </h1>
+                <h1 style={{
+                    color: this.state.colorText
+                }}>{this.state.text2}</h1>
+                <input type="text"   onChange={text1}/>
+            </div>
+        );
+    }
 }
 
 export default App;
